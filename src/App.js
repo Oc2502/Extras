d096c18f56e68c30a52910fa39b12cccfbd1398b
elinoro <o.elinor.91@gmail.com>
2020-11-28 22:20:06 +0200

    added logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './App.css';
+import './normalize.css';
 
 const App = () => {
   return(
-    <div>Extras</div>
+    <div><img src={require('./images/logo.png')} alt='logo' className='logo'/></div>
   )
 }
 
